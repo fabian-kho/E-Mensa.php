1cd37c3d2039f3617816d30d2924bcb17638e6b6
fabian-kho <fab.kirchhoff98@gmail.com>
2020-12-14 17:47:48 +0100

    Login hinzugefügt

--- emensa/public/index.php
+++ emensa/public/index.php
@@ -158,6 +158,30 @@ class FrontController
     }
 }
 
+/***PDO-Connection**************************/
+function connectdb_PDO()
+{
+    $host = 'localhost';
+    $db = 'e_mensa';        //Datenbank
+    $user = 'root';
+    $pass = 'Leonie3009';
+    $port = "3306";
+    $charset = 'utf8mb4';
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (PDOException $e) {
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+    }
+    return $pdo;
+}
+
 function connectdb() {
     $config = include CONFIG_DB;
     $link = mysqli_connect('p:'. $config['host'], // Host der Datenbank (p: Persistent connection)
